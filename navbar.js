6dcbaf3d3e255d59ce0d07558179f834ae63b9d3
Grigory Moshkevich <ilcostuff@gmail.com>
2017-09-12 00:05:49 +0300

    4

--- navbar.js
+++ navbar.js
@@ -3,17 +3,35 @@ $(document).ready(function () {
   $(window).resize(function() {
     var dropdownLeft = $('#dropdownTrigger').offset().left - ( $('.dropdown').width() - $('#dropdownTrigger').width() ) / 2;
     if ($('.dropdown').hasClass('active')) {  
-      $('.dropdown').css({transform: 'translateX(' + dropdownLeft + 'px)'})
+      $('.dropdown').css({transform: 'translateX(' + dropdownLeft + 'px)'});
       //нашел лучше решение ^
       /*$('.dropdown').offset({
         left: dropdownLeft
       }) */
     }
-  })
-  
-  $(document).click(function(e) {
+  });
+
+  var count = 0;  
+  $('#dropdownTrigger, .dropdown').mouseenter(function() {
+    count++;
+    $('.dropdown').removeClass('closed').addClass('active');
+    $(window).resize();
+    $('#dropdownTrigger').click(function() {
+      $('.dropdown').removeClass('active').addClass('closed');
+    });
+  });
+
+  $('#dropdownTrigger, .dropdown').mouseleave(function() {
+    count--;
+    if (count == 0) {
+      $('.dropdown').removeClass('active').addClass('closed');
+    }
+  });
+
+  //для управления dropdown с помощью кликов
+  /* $(document).hover(function(e) {
     if ($('.dropdown').hasClass('active')) {
-      if (!$('.dropdown').is(e.target) && $('.dropdown').has(e.target).length === 0) {
+      if (!$('.dropdown').is(e.target) && $('.dropdown').has(e.target).length === 0) && (!$('#dropdownTrigger').is(e.target) && $('#dropdownTrigger').has(e.target).length === 0) {
         $('.dropdown').removeClass('active').addClass('closed');
       }
     }
@@ -23,6 +41,6 @@ $(document).ready(function () {
       }
       $(window).resize();
     }
-  })
+  }) */
   
 });

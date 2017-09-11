9ba03a4eb509dceba59f87ea0974d3db225dfc46
Grigory Moshkevich <ilcostuff@gmail.com>
2017-09-11 20:02:27 +0300

    2

--- navbar.js
+++ navbar.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
 
   $(window).resize(function() {
-    if ($(".dropdown").hasClass("active")) {  
-      $('.dropdown').offset({
-        left: $("#dropdownTrigger").offset().left
-      })
+    var dropdownLeft = $('#dropdownTrigger').offset().left - ( $('.dropdown').width() - $('#dropdownTrigger').width() ) / 2;
+    if ($('.dropdown').hasClass('active')) {  
+      $('.dropdown').css({transform: 'translateX(' + dropdownLeft + 'px)'})
+      //нашел лучше решение ^
+      /*$('.dropdown').offset({
+        left: dropdownLeft
+      }) */
     }
   })
   
   $(document).click(function(e) {
-    if ($(".dropdown").hasClass("active")) {
-      if (!$(".dropdown").is(e.target) && $(".dropdown").has(e.target).length === 0) {
-        $(".dropdown").removeClass("active").addClass("closed");
+    if ($('.dropdown').hasClass('active')) {
+      if (!$('.dropdown').is(e.target) && $('.dropdown').has(e.target).length === 0) {
+        $('.dropdown').removeClass('active').addClass('closed');
       }
     }
     else {
-      if ($("#dropdownTrigger").is(e.target)) {
-        $(".dropdown").removeClass("closed").addClass("active");
+      if ($('#dropdownTrigger').is(e.target)) {
+        $('.dropdown').removeClass('closed').addClass('active');
       }
       $(window).resize();
     }

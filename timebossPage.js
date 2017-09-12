a00a18ee009cd6a6d4251d60cc2cbd5bfdc324b6
Grigory Moshkevich <ilcostuff@gmail.com>
2017-09-12 20:11:29 +0300

    5

---
+++ timebossPage.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+  $(window).resize(function() {
+    var dropdownLeft = $('#dropdownTrigger').offset().left - ( $('.dropdown').width() - $('#dropdownTrigger').width() ) / 2;
+    if ($('.dropdown').hasClass('active')) {  
+      $('.dropdown').css({transform: 'translateX(' + dropdownLeft + 'px)'});
+      //нашел лучше решение ^
+      /*$('.dropdown').offset({
+        left: dropdownLeft
+      }) */
+    }
+  });
+
+  var count = 0;  
+  $('#dropdownTrigger, .dropdown').mouseenter(function() {
+    count++;
+    $('.dropdown').removeClass('closed').addClass('active');
+    $(window).resize();
+  });
+
+  $('#dropdownTrigger, .dropdown').mouseleave(function() {
+    count--;
+    if (count == 0) {
+      $('.dropdown').removeClass('active').addClass('closed');
+    }
+  });
+
+  $('#dropdownTrigger').click(function() {
+    if ($('.dropdown').hasClass('active')) {
+      $('.dropdown').removeClass('active').addClass('closed');
+    }
+    else if ($('.dropdown').hasClass('closed')) {
+      $('.dropdown').removeClass('closed').addClass('active');
+    }
+  });
+  
+
+  //для управления dropdown с помощью кликов
+  /* $(document).hover(function(e) {
+    if ($('.dropdown').hasClass('active')) {
+      if (!$('.dropdown').is(e.target) && $('.dropdown').has(e.target).length === 0) && (!$('#dropdownTrigger').is(e.target) && $('#dropdownTrigger').has(e.target).length === 0) {
+        $('.dropdown').removeClass('active').addClass('closed');
+      }
+    }
+    else {
+      if ($('#dropdownTrigger').is(e.target)) {
+        $('.dropdown').removeClass('closed').addClass('active');
+      }
+      $(window).resize();
+    }
+  }) */
+  
+});
